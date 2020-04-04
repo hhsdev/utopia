c51c3ab5786d60d4af5392ed32f65225a15e5932
hhsdev <heinhtetsoe.dev@gmail.com>
2020-04-04 16:42:06 +0630

    SimpleUnicodeBuffer: Implement push_back

--- include/utopia/simple_unicode_buffer.h
+++ include/utopia/simple_unicode_buffer.h
@@ -1,30 +1,59 @@
 #ifndef UTOPIA_BUFFER_H
 #define UTOPIA_BUFFER_H
+#include <memory>
 #include <type_traits>
 #include "hhs/has_member_function.h"
 #include "utopia/index_wrapper.h"
 #include "utopia/unicode_buffer_interface.h"
 
 namespace utopia {
-
+/**
+ * Plain old vector-like buffer.
+ */
 class SimpleUnicodeBuffer : public UnicodeBufferInterface<SimpleUnicodeBuffer> {
+  using AllocatorTraits = std::allocator_traits<std::allocator<uint32_t>>;
  public:
-  SimpleUnicodeBuffer() = default;
-  SimpleUnicodeBuffer(size_t size)
-      : mBuffer(new uint32_t[size]()), mSize(size) {}
+  SimpleUnicodeBuffer() : mBuffer(nullptr), mCapacity(0), mSize(0) {}
+
+  SimpleUnicodeBuffer(size_t size) : SimpleUnicodeBuffer() {
+    mBuffer = AllocatorTraits::allocate(mAllocator, size);
+    AllocatorTraits::construct(mAllocator, mBuffer);
+    mSize = mCapacity = size;
+  }
 
   uint32_t operator[](size_t index) const { return mBuffer[index]; }
 
   auto operator[](size_t index) {
     return IndexWrapper<SimpleUnicodeBuffer>(*this, index);
   }
+
   uint32_t get(size_t index) const { return mBuffer[index]; }
   void set(size_t index, uint32_t value) { mBuffer[index] = value; }
 
   size_t size() const { return mSize; }
 
+  void push_back(const uint32_t value) {
+    if (mSize == mCapacity) {
+      if (!mCapacity) ++mCapacity;
+      mCapacity *= 2;
+      auto newBuffer = AllocatorTraits::allocate(mAllocator, mCapacity);
+      for (unsigned i = 0; i < mSize; ++i) {
+        AllocatorTraits::construct(mAllocator, newBuffer + i, *(mBuffer + i));
+      }
+      mBuffer = newBuffer;
+    }
+    AllocatorTraits::construct(mAllocator, mBuffer + mSize, value);
+    ++mSize;
+  }
+
+  ~SimpleUnicodeBuffer() {
+    AllocatorTraits::destroy(mAllocator, mBuffer);
+    AllocatorTraits::deallocate(mAllocator, mBuffer, mCapacity);
+  }
  private:
+  std::allocator<uint32_t> mAllocator;
   uint32_t* mBuffer;
+  size_t mCapacity;
   size_t mSize;
 };
 }  // namespace utopia
--- tests/unit_tests/simple_unicode_buffer.test.cpp
+++ tests/unit_tests/simple_unicode_buffer.test.cpp
@@ -4,25 +4,35 @@
 using namespace utopia;
 constexpr char const* TAG = "[SimpleUnicodeBuffer]";
 TEST_CASE("constructors work", TAG) {
-	SimpleUnicodeBuffer buffer(10);
-	REQUIRE(buffer.size() == 10);
+  SimpleUnicodeBuffer buffer(10);
+  REQUIRE(buffer.size() == 10);
 }
 
 TEST_CASE("set and get work", TAG) {
-	SimpleUnicodeBuffer buffer(1);
-	buffer.set(0, 0xdeadbeef);
+  SimpleUnicodeBuffer buffer(1);
+  buffer.set(0, 0xdeadbeef);
 
-	REQUIRE(buffer.get(0) == 0xdeadbeef);
+  REQUIRE(buffer.get(0) == 0xdeadbeef);
 }
 
 TEST_CASE("index operator works", TAG) {
-	SimpleUnicodeBuffer buffer(5);
+  SimpleUnicodeBuffer buffer(5);
 
-	buffer.set(3, 0xdeadbeef);
-	REQUIRE(buffer[3] == 0xdeadbeef);
+  buffer.set(3, 0xdeadbeef);
+  REQUIRE(buffer[3] == 0xdeadbeef);
 
-	buffer[2] = 0xcafebabe;
-	REQUIRE(buffer[2] == 0xcafebabe);
+  buffer[2] = 0xcafebabe;
+  REQUIRE(buffer[2] == 0xcafebabe);
 }
 
+TEST_CASE("push_back works", TAG) {
+  SimpleUnicodeBuffer buffer;
 
+  for (unsigned i = 0; i < 100; ++i) {
+    buffer.push_back(i);
+  }
+
+  for (unsigned i = 0; i < 100; ++i) {
+    REQUIRE(buffer[i] == i);
+  }
+}
