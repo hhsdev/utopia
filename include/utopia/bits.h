4d5ad9aa862e8aacc92a1a8d61feae850577c816
hhsdev <heinhtetsoe.dev@gmail.com>
2020-04-09 15:28:39 +0630

    bits: Implement countLeadingZeroes and countLeadingOnes

--- include/utopia/bits.h
+++ include/utopia/bits.h
@@ -2,9 +2,44 @@
 #define UTOPIA_BITS_H
 #include <climits>
 #include <iostream>
+#include <type_traits>
 
 namespace utopia {
 namespace bits {
+
+template <typename Unsigned>
+std::string toBinary(Unsigned bits) {
+  constexpr auto BIT_SIZE = sizeof(Unsigned) * CHAR_BIT;
+  std::string ret(BIT_SIZE, '0');
+  for (auto i = ret.size(); i != 0; --i) {
+    if (bits & Unsigned(1))
+      ret[i - 1] = '1';
+    bits = static_cast<Unsigned>(bits >> 1);
+  }
+  return ret;
+}
+
+template <typename Unsigned>
+inline typename std::enable_if<std::is_unsigned_v<Unsigned>, unsigned>::type
+countLeadingOnes(Unsigned bits) {
+  constexpr auto BIT_SIZE = sizeof(Unsigned) * CHAR_BIT;
+  Unsigned mask = static_cast<Unsigned>(1) << (BIT_SIZE - 1);
+
+  unsigned leadingOneCount = 0;
+
+  while ((bits & mask) != 0) {
+    ++leadingOneCount;
+    mask = static_cast<Unsigned>(static_cast<Unsigned>(mask) >> 1);
+  }
+  return leadingOneCount;
+}
+
+template <typename Unsigned>
+inline typename std::enable_if<std::is_unsigned_v<Unsigned>, unsigned>::type
+countLeadingZeroes(Unsigned bits) {
+  const Unsigned invertedBits = static_cast<Unsigned>(~bits);
+  return countLeadingOnes<Unsigned>(invertedBits);
+}
 
 template <typename Buffer, typename Bits>
 Buffer pack(Buffer buffer, Bits bits, uint8_t sz) {
@@ -21,13 +56,13 @@ inline bool isLeadingByte(uint8_t byte) {
 }
 
 inline uint8_t numCodeBitsInLeadingByte(uint8_t byte) {
-  if ((byte & 0b1000'0000) == static_cast<uint8_t>(0))
+  if ((byte & 0b1000'0000) == 0)
     return 7;
-  if ((byte & 0b1110'0000) == static_cast<uint8_t>(0b1100'0000))
+  if ((byte & 0b1110'0000) == 0b1100'0000)
     return 5;
-  if ((byte & 0b1111'0000) == static_cast<uint8_t>(0b1110'0000))
+  if ((byte & 0b1111'0000) == 0b1110'0000)
     return 4;
-  if ((byte & 0b1111'1000) == static_cast<uint8_t>(0b1111'0000))
+  if ((byte & 0b1111'1000) == 0b1111'0000)
     return 3;
 
   throw std::runtime_error("Should not reach here");
--- tests/unit_tests/bits.test.cpp
+++ tests/unit_tests/bits.test.cpp
@@ -1,9 +1,35 @@
 #include "catch2/catch.hpp"
 #include "utopia/bits.h"
-
+#include <iostream>
 using namespace utopia::bits;
 constexpr char const* TAG = "[bit]";
 
+TEST_CASE("countLeadingOnes works", TAG) {
+  REQUIRE(countLeadingOnes<uint8_t>(0b1110'0000U) == 3U);
+  REQUIRE(countLeadingOnes<uint8_t>(0b1110'1111U) == 3U);
+
+  REQUIRE(countLeadingOnes<uint16_t>(0b1110'0000'0000'0000U) == 3U);
+  REQUIRE(countLeadingOnes<uint16_t>(0b1110'1111'0000'0000U) == 3U);
+
+  REQUIRE(countLeadingOnes(0b1110'0000'0000'0000'0000'0000'0000'0000U) == 3U);
+  REQUIRE(countLeadingOnes(0b1110'1111'0000'0000'0000'0000'0000'0000U) == 3U);
+
+  REQUIRE(countLeadingOnes<uint8_t>(0b0111'1111) == 0U);
+}
+
+TEST_CASE("countLeadingZeroes works", TAG) {
+  REQUIRE(countLeadingZeroes<uint8_t>(0b0001'1111U) == 3U);
+  REQUIRE(countLeadingZeroes<uint8_t>(0b0001'0000U) == 3U);
+
+  REQUIRE(countLeadingZeroes<uint16_t>(0b0001'1111'1111'1111U) == 3U);
+  REQUIRE(countLeadingZeroes<uint16_t>(0b0001'0000'0000'0000U) == 3U);
+
+  REQUIRE(countLeadingZeroes(0b0001'1111'1111'1111'1111'1111'1111'1111U) == 3U);
+  REQUIRE(countLeadingZeroes(0b0001'0000'0000'0000'0000'0000'0000'0000U) == 3U);
+
+  REQUIRE(countLeadingZeroes<uint32_t>(0b0) == 32U);
+  REQUIRE(countLeadingZeroes<uint8_t>(0b1000'0000) == 0U);
+}
 TEST_CASE("pack works", TAG) {
   uint32_t buffer = 0U;
 
